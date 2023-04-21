7e3ea1b2ff4312a9db8e34c1b6b5952cfa44464f
iuejeong <dmlgus3585@naver.com>
2023-04-21 09:00:09 +0900

    bookmanagement 회원가입, 로그인 성공, 메인 페이지 생성

--- book_management/src/pages/Register/Register.js
+++ book_management/src/pages/Register/Register.js
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import React, { useState } from 'react';
-import { Await, Link } from 'react-router-dom';    // router 안에서 이동할 때 사용됨.
+import { Link, useNavigate } from 'react-router-dom';    // router 안에서 이동할 때 사용됨.
 import { FiLock, FiUser, } from 'react-icons/fi';
 import { BiRename } from 'react-icons/bi';
 import LoginInput from '../../components/UI/Login/LoginInput/LoginInput';
@@ -78,6 +78,8 @@ const errorMsg = css`
 `;
 
 const Register = () => {
+    const navigate = useNavigate();
+
     const [registerUser, setRegisterUser] = useState({email:"", password: "", name: ""});
     const [errorMessages, setErrorMessages] = useState({email: "", password: "", name: ""});
 
@@ -96,16 +98,18 @@ const Register = () => {
             }
         }
         try {
-            const response = await axios.post("http://localhost:8080/auth/signup", JSON.stringify(data), option);        // promise를 return 하는 것임 post 내용이 정상 실행이면 then, 아니면 catch
+            await axios.post("http://localhost:8080/auth/signup", JSON.stringify(data), option);        // promise를 return 하는 것임 post 내용이 정상 실행이면 then, 아니면 catch
             setErrorMessages({email: "", password: "", name: ""});
-            
+            alert("회원가입 성공");
+            navigate("/login");
+
         } catch(error) {
             setErrorMessages({email: "", password: "", name: "", ...error.response.data.errorData});
         }
         // 동기는 요청을 날려놓고 응답이 올 때까지 가만히 기다린다.
         // 비동기는 요청 날려놓고 다른 할 일을 하는 것. 이유는 싱글 쓰레드이기 때문에
         // 웹 페이지에서는 오래걸리는 데이터는 나중에 띄우는 것이 비동기, 위에서부터 순서대로 띄우는 것이 동기
-        // await을 해놓으면 then이 실행이 되는 것임.
+        // await을 해놓으면 then이 실행이 되는 것임.`
         // await을 쓰려면 함수에 async를 써줘야 함.
         // await의 return값이 있으면 변수에 담아줄 수 있다.
         // const response = await axios.post("http://localhost:8080/auth/signup", JSON.stringify(data), option);        // promise를 return 하는 것임 post 내용이 정상 실행이면 then, 아니면 catch
